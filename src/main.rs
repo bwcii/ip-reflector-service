932e67a417694ae6974533afaa1710b4da209860
Benjamin Crouch II <benjamin.crouch.ii@bwcii.com>
2023-08-31 00:12:13 -0500

    Updated the listening address

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(handler));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
