91ea6ddf2f2324f85f170d26186f1af39dd9762a
Benjamin Crouch II <141606584+bwcii@users.noreply.github.com>
2023-09-12 15:51:31 +0000

    Added command line args for IP and port

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use axum::{
     extract::ConnectInfo, 
     routing::get, Router,
-    http::Request,
-    body::Body,
 };
-use std::net::SocketAddr;
+use std::net::{SocketAddr, Ipv4Addr};
+use clap::Parser;
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("0.0.0.0"))]
+    listening_ip: String,
+
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
+
+    let args = Args::parse();
+    let listening_ip_addr: Ipv4Addr = args.listening_ip.parse().expect("Unable to parse IP address");
+
+    println!("Listening IP : {}", args.listening_ip);
+    if args.listening_ip == String::from("0.0.0.0") {
+        println!("CAUTION: Using 0.0.0.0 for the listening IP address effectively listens on ALL IP addresses.");
+    }
+    println!("Listening Port : {}", args.port);
+
     let app = Router::new()
-    .route("/", get(handler))
-    .route("/test", get(request));
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    .route("/", get(handler));
+    let addr = SocketAddr::from((listening_ip_addr, args.port));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
@@ -22,8 +38,4 @@ async fn main() {
 
 async fn handler(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
     format!("{}\r\n", addr.ip())
-}
-
-async fn request(request: Request<Body>) {
-    println!("{:?}", request);
 }
