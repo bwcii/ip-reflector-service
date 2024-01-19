76d665418d392643178b35a4f008d0feb72876c6
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-19 16:10:56 +0000

    Updated the port to 8080

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use routes::create_routes;
 pub async fn run() {
     let app = create_routes();
 
-    let binding_socket = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
+    let binding_socket = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     axum::serve(binding_socket, app)
         .await
