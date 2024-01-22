e5eb7700f170b856f091119e64ff0997dd2f3b0b
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-22 04:27:48 +0000

    I think I got basic SSR working

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,7 +1,28 @@
 use axum::http::HeaderMap;
+use handlebars::{
+    Handlebars,
+    DirectorySourceOptions,
+};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct ClientIP {
+    public_ip: String,
+}
 
 pub async fn handler(headers: HeaderMap) -> String {
+//pub async fn handler() -> String {
+    let mut handlebars = Handlebars::new();
+    let directory_options = DirectorySourceOptions{tpl_extension: ".hbs".to_string(), hidden: false, temporary: false};
+    
+    handlebars.register_templates_directory("templates", directory_options).unwrap();
+
     let message = headers.get("X-Forwarded-For").unwrap();
     let message_string = message.to_str().unwrap().to_owned();
-    "current IP: ".to_owned() + &message_string
+    let client_ip = ClientIP{public_ip: message_string};
+    println!("{}", client_ip.public_ip);
+    //"current IP: ".to_owned() + &message_string
+
+    let html = handlebars.render("index", &client_ip).unwrap();
+    html
 }
