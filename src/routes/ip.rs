c40fc5d910b14e0f636a09e97d897c2243b916df
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-24 19:08:24 +0000

    starting the process of adding the api

--- src/routes/ip.rs
+++ src/routes/ip.rs
@@ -2,16 +2,16 @@ use axum::{http::HeaderMap, Json};
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct Data {
+pub struct IPData {
     ip_address: String,
 }
 
 
-pub async fn ip(headers: HeaderMap) -> Json<Data> {
+pub async fn ip(headers: HeaderMap) -> Json<IPData> {
 
     let ipaddr = headers.get("X-Forwarded-For").unwrap();
 
-    let data: Data = Data {
+    let data: IPData = IPData {
         ip_address: ipaddr.to_str().unwrap().to_string(),
     };
     Json(data)
